742d3b7c2805fd7a4c8a3197522d69f2180001fc
QuantaJi <quanta.guangda.ji@gmail.com>
2024-02-10 08:37:53 +0100

    downsampling check

---
+++ labelmaker_scripts/check_downsampling.py
@@ -0,0 +1,45 @@
+import argparse
+from pathlib import Path
+
+
+
+
+def check(
+    workspace: str
+):
+    workspace = Path(workspace)
+
+    
+
+    if tgt_label_file.exists() and tgt_pcd_file.exists():
+        return
+    else:
+        print(f"{str(workspace)} is NOT finished!")
+
+    
+
+
+def arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--workspace",
+        type=str,
+        required=True,
+        help='Path to workspace directory. There should be a "color" folder inside.',
+    )
+    parser.add_argument(
+        "--voxel_size",
+        type=float,
+        default="0.02",
+        help="Name of files to save the labels",
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = arg_parser()
+    check(
+        workspace=args.workspace,
+        
+    )
+
---
+++ labelmaker_scripts/check_downsampling.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/bash
+#SBATCH --job-name="check_downsampling"
+#SBATCH --output=check_downsampling_%j.out
+#SBATCH --time=48:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=16G
+
+# exit when any command fails
+# set -e
+
+# decide which version of python cuda pytorch torchvision to use
+# if [ -z "$1" ]; then
+#   exit 1
+# else
+#   start_group=$1
+# fi
+
+# if [ -z "$2" ]; then
+#   end_group=$start_group
+# else
+#   end_group=$2
+# fi
+
+start_group=0
+end_group=101
+
+echo $start_group $end_group
+
+module load gcc/11.4.0 python
+
+root_dir=/cluster/project/cvg/labelmaker/ARKitScene_LabelMaker
+
+input="/cluster/home/guanji/LabelMaker/pipeline/arkitscenes_info.csv"
+while IFS= read -r line; do
+    csv_line=($(echo $line | tr "," "\n"))
+    video_id=${csv_line[0]}
+    visit_id=${csv_line[1]}
+    fold=${csv_line[2]}
+    num_images=${csv_line[3]}
+    group=${csv_line[4]}
+    group_int=${group:0:4}
+    target_dir=$root_dir/$fold/$video_id
+    if [[ 10#$group_int -ge 10#$start_group ]] && [[ 10#$group_int -le 10#$end_group ]]; then
+        python labelmaker_scripts/check_downsampling.py --workspace $target_dir
+    fi
+done <"$input"
+
