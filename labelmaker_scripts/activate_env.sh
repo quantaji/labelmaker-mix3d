95ab26ed478967d696b9ba03d77bfdd3b57a7c04
QuantaJi <quanta.guangda.ji@gmail.com>
2024-02-10 07:20:24 +0100

    test

---
+++ labelmaker_scripts/activate_env.sh
@@ -0,0 +1,11 @@
+module load gcc/11.4.0 cuda/12.1.1 eth_proxy
+export PATH="/cluster/project/cvg/labelmaker/miniconda3/bin:\${PATH}"
+env_name=labelmaker-mix3d
+eval "$(conda shell.bash hook)"
+conda activate $env_name
+conda_home="$(conda info | grep "active env location : " | cut -d ":" -f2-)"
+conda_home="${conda_home#"${conda_home%%[![:space:]]*}"}"
+
+export CUDA_HOST_COMPILER="$conda_home/bin/gcc"
+export CUDA_PATH="$conda_home"
+export CUDA_HOME=$CUDA_PATH
