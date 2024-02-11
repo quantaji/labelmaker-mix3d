38ecab05c7dc3e976d526c307d91678b19e52459
QuantaJi <quanta.guangda.ji@gmail.com>
2024-02-11 05:45:14 +0100

    change gpu type

--- labelmaker_scripts/train_arkit.sh
+++ labelmaker_scripts/train_arkit.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem-per-cpu=64G
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=16G
-#SBATCH --gpus=rtx_4090:1
+#SBATCH --gpus=a100-pcie-40gb:1
 
 module purge
 module load gcc/11.4.0 cuda/12.1.1 eth_proxy
